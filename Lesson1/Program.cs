94ebea78ae072f16a45a2ca6efe7a05c90dfae23
Aleksandrs Ivanovs <alexandroff@inbox.lv>
2021-03-23 16:57:35 +0200

    # Вывод даты и времени

---
+++ Lesson1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dtnow = DateTime.Now;
+
+            Console.WriteLine("C# first program!");
+            Console.WriteLine("The time: {0:d} at {0:t}", dtnow);
+
+            Console.Write("Press <Enter> to exit... ");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+    }
+}
