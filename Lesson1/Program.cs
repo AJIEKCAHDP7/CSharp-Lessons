dea9d6f1e1560660a9932a51c29645e746eba06e
Aleksandrs Ivanovs <alexandroff@inbox.lv>
2021-03-23 17:32:39 +0200

    # 1.1. Display the number to the nearest hundredths.

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -6,9 +6,16 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            DateTime dtnow = DateTime.Now;
+            // 1. Arithmetic expressions
+            Console.WriteLine("1. Arithmetic expressions");
+            // 1.1. Display the number to the nearest hundredths.
+            Console.WriteLine("1.1. Display the number to the nearest hundredths.");
+            double dbnumber = 10.99;
+            Console.WriteLine("Decimal number: ", dbnumber);
+            Console.WriteLine("--- /// ---");
 
-            Console.WriteLine("C# first program!");
+
+            DateTime dtnow = DateTime.Now;
             Console.WriteLine("The time: {0:d} at {0:t}", dtnow);
 
             Console.Write("Press <Enter> to exit... ");
